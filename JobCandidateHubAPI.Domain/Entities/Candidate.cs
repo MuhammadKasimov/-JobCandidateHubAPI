b224de22f9ca99a18530c3e347f804a9d0f22aa5
MuhammadKasimov <muhammadmamushah.com@gmail.com>
2024-06-18 17:34:29 +0500

    DOMAIN: Created candidate entity

---
+++ JobCandidateHubAPI.Domain/Entities/Candidate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobCandidateHubAPI.Domain.Entities
+{
+    public class Candidate
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string TimeForCall { get; set; }
+        public string LinkedInUrl { get; set; }
+        public string GithubUrl { get; set; }
+        public string Comment { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
